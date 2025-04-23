fe4f8f55b792b88d4ae827c6e4be7afd884cb3fd
Nigel210717 <73235560+Gqblin@users.noreply.github.com>
2025-04-23 14:44:07 +0200

    Added weeping angel enemy

--- Assets/Scripts/WeepingAngel.cs
+++ Assets/Scripts/WeepingAngel.cs
@@ -1,16 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class WeepingAngel : MonoBehaviour
+public class Monster : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float speed = 4;
+    [SerializeField] private bool inView;
+    private NavMeshAgent navMeshAgent;
+    [SerializeField] private Transform player;
+    //private Animation anim;
+
     void Start()
     {
-        
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        //anim = GetComponent<Animation>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!inView)
+        {
+            navMeshAgent.SetDestination(player.position);
+        }
+    }
+
+    private void OnBecameVisible()
+    {
+        inView = true;
+        navMeshAgent.velocity = new Vector3(0, 0, 0);
+        navMeshAgent.ResetPath();
+        //anim.enabled = false;
+    }
+
+    private void OnBecameInvisible()
+    {
+        inView = false;
+        //anim.enabled = true;
     }
 }
